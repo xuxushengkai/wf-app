ce742ce228db3da3f8d4c6b644ca941b5947a9f1
shengjunjie <811860136@qq.com>
2019-08-22 22:07:13 +0800

    数据库修改

--- wf.sql
+++ wf.sql
@@ -173,10 +173,10 @@ CREATE TABLE `tb_user` (
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_user_login_record`;
 CREATE TABLE `tb_user_login_record` (
-  `id` bigint(20) NOT NULL,
-  `user_id` char(36) NOT NULL COMMENT '用户ID',
+  `id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键',
+  `user_id` varchar(36) NOT NULL COMMENT '用户ID',
   `user_name` varchar(100) NOT NULL DEFAULT '' COMMENT '用户名称',
-  `telephone` char(12) DEFAULT NULL COMMENT '用户手机号',
+  `telephone` varchar(11) DEFAULT NULL COMMENT '用户手机号',
   `login_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '登录时间',
   `logout_time` datetime DEFAULT NULL COMMENT '登出时间',
   `token` varchar(350) NOT NULL DEFAULT '0' COMMENT 'token',
@@ -184,8 +184,7 @@ CREATE TABLE `tb_user_login_record` (
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `is_deleted` bit(1) DEFAULT b'0' COMMENT '是否删除(0未删除 1已删除)',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户登录日志表';
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='用户登录日志表';
 -- ----------------------------
 -- Table structure for tb_user_role_relation
 -- ----------------------------
